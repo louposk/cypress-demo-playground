8e72f435220dc7265600a25ebabff1c7acdcf65a
Konstantinos Loupos <louposk@gmail.com>
2023-03-21 00:44:06 +0200

    Added before after in header

--- cypress/e2e/conduit/shared/header.cy.js
+++ cypress/e2e/conduit/shared/header.cy.js
@@ -1,12 +1,29 @@
 import menu from '../../../selectors/menu.sel'
 
+before(() => {
+    cy.log('Before')
+})
+
+after(()=>{
+    cy.log("After")
+})
+
 describe('Menu',() => {
     it('is a first test', () => {
         cy.login() 
         cy.visit('https://demo.realworld.io/')
+        cy.get(menu.signIn).contains('Sign in').should('exist').click({multiple: true})  
+        // cy.get(menu.signUp).contains('Sign up').should('exist').click({multiple: true})  
+        // cy.get(menu.signIn).contains('Sign in').should('exist').click({multiple: true})  
+        // cy.navigateTo('Sign Up')
+    }) 
+
+    it('is a second test', () => {
+        // cy.login() 
+        cy.visit('https://demo.realworld.io/#/settings')
         // cy.get(menu.signIn).contains('Sign in').should('exist').click({multiple: true})  
         // cy.get(menu.signUp).contains('Sign up').should('exist').click({multiple: true})  
         // cy.get(menu.signIn).contains('Sign in').should('exist').click({multiple: true})  
         // cy.navigateTo('Sign Up')
-    })
+    }) 
 })
